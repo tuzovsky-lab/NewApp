6b0c7a203e2a4482f980d75440f4e766e0c47cbb
TuzovskyAF <taf@artcollege.ru>
2024-02-19 11:20:36 +0300

    Добавлен код ввода данных для х.

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -10,6 +10,10 @@ namespace NewApp
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите значения параметра x = ");
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
+
         }
     }
 }
